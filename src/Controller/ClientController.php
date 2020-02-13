e4440a31671e17f2ba19cb36eeca07f57e651d7b
Nicolae <ndanut@gmail.com>
2020-02-13 16:59:02 +0100

    Diseño

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="client")
+     */
+    public function index()
+    {
+        return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/searchClient/{val}", name="searchClient")
+     */
+    public function searchClient($val){
+
+    }
+
+
+}
