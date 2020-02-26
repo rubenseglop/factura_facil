84218581263819c6eb92d0e098278781f09d7958
Fernando <fernandofelipesanchezgamez@gmail.com>
2020-02-26 20:24:46 +0100

    Comienzo Editar Empresa

---
+++ src/Form/EditCompanyType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditCompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',null, ['required' => false])
+            ->add('fiscalAddress',null, ['required' => false])
+            ->add('email',null, ['required' => false])
+            ->add('NIF',null, ['required' => false])
+            ->add('Submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
